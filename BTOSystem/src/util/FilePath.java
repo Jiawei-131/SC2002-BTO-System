e7e77b6c8cfa1a14c949f2c0779adc1eabe66672
Jiawei-131 <GUJI0005@e.ntu.edu.sg>
2025-04-16 15:29:46 +0800

    added enquiry mvc

--- BTOSystem/src/util/FilePath.java
+++ BTOSystem/src/util/FilePath.java
@@ -4,6 +4,7 @@ public interface FilePath {
 	final String filePath = "BTOSystem/src/data/";
 	final String loginFilePath=filePath+"LoginInfo.txt";
 	final String userDatabaseFilePath=filePath+"UserDatabase.txt";
+	final String enquiryDatabaseFilePath=filePath+"EnquiryDatabase.txt";
 	final String projectDatabaseFilePath=filePath+"ProjectDatabase.txt";
 	final String officerApplicationDatabaseFilePath=filePath+"OfficerApplicationDatabase.txt";
 	final String projectApplicationDatabaseFilePath=filePath+"ProjectApplicationDatabase.txt";
